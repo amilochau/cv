fa485b0d339ffd92e08baab713733839d27dd400
Antoine Milochau <48644548+amilochau@users.noreply.github.com>
2021-11-25 09:54:45 +0100

    Propose two modes (detailed and reducted) to print

--- src/components/resume/section/Call.vue
+++ src/components/resume/section/Call.vue
@@ -13,6 +13,31 @@
       class="text-body-2 text--secondary font-weight-light mb-0">
       {{ call.lastUpdate | formatUpdateDate | formatText('resume.call.lastUpdate') }}
     </p>
+    <template v-slot:actions>
+      <v-btn
+        v-if="expanded"
+        text
+        color="deep-purple accent-4"
+        @click="$emit('reduce')">
+        <v-icon left>mdi-unfold-less-horizontal</v-icon>
+        {{ $t('resume.call.reduce') }}
+      </v-btn>
+      <v-btn
+        v-else
+        text
+        color="deep-purple accent-4"
+        @click="$emit('expand')">
+        <v-icon left>mdi-unfold-more-horizontal</v-icon>
+        {{ $t('resume.call.expand') }}
+      </v-btn>
+      <v-btn
+        text
+        color="deep-purple accent-4"
+        @click="print">
+        <v-icon left>mdi-printer</v-icon>
+        {{ $t('resume.call.print') }}
+      </v-btn>
+    </template>
   </v-banner>
 </template>
 
@@ -22,5 +47,11 @@ import { Component, Prop, Vue } from 'vue-property-decorator'
 @Component
 export default class KeyWords extends Vue {
   @Prop({ type: Object, required: true }) public call!: any;
+
+  @Prop({ type: Boolean, required: true }) public expanded!: boolean;
+
+  public print () {
+    window.print()
+  }
 }
 </script>
