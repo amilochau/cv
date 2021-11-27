c24103b54428952aa79052c152a2b3b02c5dbc31
Antoine Milochau <48644548+amilochau@users.noreply.github.com>
2021-11-27 01:26:09 +0100

    Add tags on experiences (#87)

--- src/components/resume/section/KeyWords.vue
+++ src/components/resume/section/KeyWords.vue
@@ -7,18 +7,29 @@
       <v-icon left>mdi-filter</v-icon>
       {{ $t('resume.keyWords.title') }}
     </v-card-title>
-    <v-card-text>
+    <v-card-text class="text-center">
       <v-chip-group
         column
         multiple
         active-class="primary--text">
         <v-chip
           v-for="(topic, i) in topics"
-          :key="i">
+          :key="i"
+          outlined
+          small
+          class="mr-2 mb-1 v-chip--tile">
           {{ topic.title }}
         </v-chip>
       </v-chip-group>
     </v-card-text>
+    <v-card-actions v-if="!expanded">
+      <v-btn
+        text
+        @click="$emit('expand')">
+        <v-icon left>mdi-unfold-more-horizontal</v-icon>
+        {{ $t('resume.keyWords.expand') }}
+      </v-btn>
+    </v-card-actions>
   </v-card>
 </template>
 
@@ -28,5 +39,7 @@ import { Component, Prop, Vue } from 'vue-property-decorator'
 @Component
 export default class KeyWords extends Vue {
   @Prop({ type: Array, required: true }) public topics!: [];
+
+  @Prop({ type: Boolean, required: true }) public expanded!: boolean;
 }
 </script>
